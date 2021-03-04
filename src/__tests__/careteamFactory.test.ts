fe6bf550988b49da20014eb2b0c174d6ee434c9e
Christopher Manzi <chris@imaware.health>
2021-03-04 12:43:56 -0600

    feat: adds CareTeam generator and factory

---
+++ src/__tests__/careteamFactory.test.ts
@@ -0,0 +1,28 @@
+import {R4} from '@ahryman40k/ts-fhir-types';
+import {expect} from 'chai';
+import {careTeamFactory} from '../factory/fhir/r4/careteam';
+import {uuidV4Regex} from './helpers/patterns';
+
+describe('CareTeamFactory', () => {
+  it('correctly creates an careteam with no defaults', async () => {
+    const ct = careTeamFactory.build();
+    expect(ct.id).to.exist;
+    expect(ct.id).to.match(uuidV4Regex);
+    expect(ct.resourceType).to.exist;
+    expect(ct.resourceType).to.equal('CareTeam');
+    expect(ct.status).to.exist;
+  });
+  it('correctly creates an careteam with defaults', async () => {
+    const preSets = {
+      id: '9d5058fa-4ea5-47c9-bf62-d9b8426e4fe5',
+      status: 'proposed' as R4.CareTeamStatusKind,
+    };
+    const ct = careTeamFactory.build(preSets);
+    expect(ct.id).to.exist;
+    expect(ct.id).to.equal(preSets.id);
+    expect(ct.resourceType).to.exist;
+    expect(ct.resourceType).to.equal('CareTeam');
+    expect(ct.status).to.exist;
+    expect(ct.status).to.equal(ct.status);
+  });
+});
